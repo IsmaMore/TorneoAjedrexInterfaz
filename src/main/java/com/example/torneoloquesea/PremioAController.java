4513d6012edb437efc1c5aee2e6bd86bc95269c0
IsmaMore <xismahackerx@gmail.com>
2023-05-31 11:33:26 +0200

    añadido crear fichero con datos premio

--- src/main/java/com/example/torneoloquesea/PremioAController.java
+++ src/main/java/com/example/torneoloquesea/PremioAController.java
@@ -7,11 +7,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -26,6 +27,11 @@ public class PremioAController extends  ModificarController implements Initializ
             e.printStackTrace();
         }
     }
+
+    private ArrayList<Premio> premios;
+
+    @FXML
+    private Label labelExA;
 
     @FXML
     private TableView<Premio> tablaPremioA = new TableView<>();
@@ -42,6 +48,57 @@ public class PremioAController extends  ModificarController implements Initializ
     @FXML
     private TableColumn<Premio, String> clmCantidadA;
 
+    @FXML
+    protected void exportarA(){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("PremiosA.txt"));
+            int espNombre = 40, espTipo_Premio = 25, espInt = 8;
+            bw.write(" Posicion | Nombre                                   | Tipo_Premio               | Cantidad");
+            bw.newLine();
+            bw.write(" ---------|------------------------------------------|---------------------------|---------");
+            bw.newLine();
+            for (Premio premio: premios){
+                int position = premio.getPosicion();
+                bw.write(" " + position);
+                if (position < 10){
+                    bw.write("       ");
+                }else if (position < 100){
+                    bw.write("      ");
+                }else if (position < 1000){
+                    bw.write("     ");
+                }
+                bw.write(" | ");
+                String nombre = premio.getNombre();
+                bw.write(nombre);
+                for (int i = 1; i <= espNombre - nombre.length(); i++ ){
+                    bw.write(" ");
+                }
+                bw.write(" | ");
+                String tipo_Premio = premio.getTipo_Premio();
+                bw.write(tipo_Premio);
+                for (int i = 1; i <= espTipo_Premio - tipo_Premio.length(); i++ ){
+                    bw.write(" ");
+                }
+                bw.write(" | ");
+                String cantidad = premio.getCantidad();
+                bw.write(" " + cantidad);
+                if (cantidad.length() < 3){
+                    bw.write("      ");
+                }else if (cantidad.length() < 4){
+                    bw.write("     ");
+                }else if (cantidad.length() < 5){
+                    bw.write("    ");
+                }
+                bw.newLine();
+            }
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        labelExA.setText("Se ha exportado la tabla de premios.");
+        labelExA.setVisible(true);
+    }
+
     @Override
     protected void volverA() throws IOException {
         super.volverA();
@@ -55,8 +112,10 @@ public class PremioAController extends  ModificarController implements Initializ
         clmTipoA.setCellValueFactory(new PropertyValueFactory<>("Tipo_Premio"));
         clmCantidadA.setCellValueFactory(new PropertyValueFactory<>("Cantidad"));
         ObservableList<Premio> ob = FXCollections.observableArrayList();
-        ob.addAll(Premio.obtenerPremios(cnxA));
+        premios = Premio.obtenerPremios(cnxA);
+        ob.addAll(premios);
         tablaPremioA.setItems(ob);
+        labelExA.setVisible(false);
     }
 }
 
--- src/main/java/com/example/torneoloquesea/PremioBController.java
+++ src/main/java/com/example/torneoloquesea/PremioBController.java
@@ -7,11 +7,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -28,6 +31,8 @@ public class PremioBController extends  ModificarController implements Initializ
         }
     }
 
+    private ArrayList<Premio> premios;
+
     @FXML
     private TableView<Premio> tablaPremioB = new TableView<>();
 
@@ -43,6 +48,67 @@ public class PremioBController extends  ModificarController implements Initializ
     @FXML
     private TableColumn<Premio, String> clmCantidadB;
 
+    @FXML
+    private Label labelExB;
+
+    @FXML
+    protected void exportarB(){
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("PremiosB.txt"));
+            int espNombre = 40, espTipo_Premio = 25, espInt = 8;
+            bw.write(" Posicion | Nombre                                   | Tipo_Premio               | Cantidad");
+            bw.newLine();
+            bw.write(" ---------|------------------------------------------|---------------------------|---------");
+            bw.newLine();
+            for (Premio premio: premios){
+                int position = premio.getPosicion();
+                bw.write(" " + position);
+                if (position < 10){
+                    bw.write("       ");
+                }else if (position < 100){
+                    bw.write("      ");
+                }else if (position < 1000){
+                    bw.write("     ");
+                }
+                bw.write(" | ");
+                String nombre = premio.getNombre();
+                bw.write(nombre);
+                for (int i = 1; i <= espNombre - nombre.length(); i++ ){
+                    bw.write(" ");
+                }
+                bw.write(" | ");
+                String tipo_Premio = premio.getTipo_Premio();
+                if (!(tipo_Premio == null)){
+                    bw.write(tipo_Premio);
+                    for (int i = 1; i <= espTipo_Premio - tipo_Premio.length(); i++ ){
+                        bw.write(" ");
+                    }
+                }else {
+                    for (int i = 1; i <= espTipo_Premio; i++ ){
+                        bw.write(" ");
+                    }
+                }
+
+                bw.write(" | ");
+                String cantidad = premio.getCantidad();
+                bw.write(" " + cantidad);
+                if (cantidad.length() < 3){
+                    bw.write("      ");
+                }else if (cantidad.length() < 4){
+                    bw.write("     ");
+                }else if (cantidad.length() < 5){
+                    bw.write("    ");
+                }
+                bw.newLine();
+            }
+            bw.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        labelExB.setText("Se ha exportado la tabla de premios.");
+        labelExB.setVisible(true);
+    }
+
     @Override
     protected void volverB() throws IOException {
         super.volverB();
@@ -56,8 +122,10 @@ public class PremioBController extends  ModificarController implements Initializ
         clmTipoB.setCellValueFactory(new PropertyValueFactory<>("Tipo_Premio"));
         clmCantidadB.setCellValueFactory(new PropertyValueFactory<>("Cantidad"));
         ObservableList<Premio> ob = FXCollections.observableArrayList();
-        ob.addAll(Premio.obtenerPremios(cnxB));
+        premios = Premio.obtenerPremios(cnxB);
+        ob.addAll(premios);
         tablaPremioB.setItems(ob);
+        labelExB.setVisible(false);
     }
 }
 
